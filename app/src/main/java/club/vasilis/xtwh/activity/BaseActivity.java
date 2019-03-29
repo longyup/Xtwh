f56943ecef42315beeee6c61828aeeacdf915e87
Vasilis <799453724@qq.com>
2019-03-29 12:41:39 +0800

    增加测试activity

---
+++ app/src/main/java/club/vasilis/xtwh/activity/BaseActivity.java
@@ -0,0 +1,63 @@
+package club.vasilis.xtwh.activity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import club.vasilis.xtwh.ActivityCollector;
+
+public class BaseActivity extends AppCompatActivity {
+    private ForceOfflineReceiver receiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("club.vasilis.xtwh.FORE_OFFLIN");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (receiver != null) {
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
+    class ForceOfflineReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+         /*   AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("您被强制下线了，请重新登陆。");
+            builder.setCancelable(false);
+            builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //注销所有活动
+                    ActivityCollector.finishAll();
+                    Intent i = new Intent(context, LoginActivity.class);
+                    context.startActivity(i);
+                }
+            });
+            builder.show();*/
+        }
+    }
+}
