80fb6392dec0da4ca9af243e0eb35227fc9e4654
Jim <1225469544@qq.com>
2019-04-28 22:05:02 +0800

    修改个人信息界面第三次提交

--- app/src/main/java/club/vasilis/xtwh/activity/MyMsgPersonageActivity.java
+++ app/src/main/java/club/vasilis/xtwh/activity/MyMsgPersonageActivity.java
@@ -1,14 +1,15 @@
 package club.vasilis.xtwh.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import club.vasilis.xtwh.R;
 
 public class MyMsgPersonageActivity extends BaseActivity {
 
+    private TextView tv_resetPed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +17,19 @@ public class MyMsgPersonageActivity extends BaseActivity {
         findViewById(R.id.my_msg_personage_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MyMsgPersonageActivity.this,"You have clicked...",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MyMsgPersonageActivity.this,"You have clicked...退出账号",Toast.LENGTH_SHORT).show();
             }
         });
+
+        findViewById(R.id.my_msg_personage_user_reset_pwd).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MyMsgPersonageActivity.this,"You have clicked...修改密码",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        tv_resetPed = findViewById(R.id.my_msg_personage_account_id);
+        tv_resetPed.setText(String.valueOf(BaseActivity.myUser.getAccount()));
     }
 }
--- app/src/main/java/club/vasilis/xtwh/adapter/MyMsgOtherAdapter.java
+++ app/src/main/java/club/vasilis/xtwh/adapter/MyMsgOtherAdapter.java
@@ -10,73 +10,67 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
-
 import club.vasilis.xtwh.R;
-import club.vasilis.xtwh.domain.MyMsgOther;
 
 public class MyMsgOtherAdapter extends RecyclerView.Adapter<MyMsgOtherAdapter.ViewHolder> {
 
     private static final String TAG = "MyMsgOtherAdapter";
-    private List<MyMsgOther> mOtherList;
+    private String[] mOtherList;
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, final int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.my_msg_other_content_item,viewGroup,false);
         final ViewHolder holder = new ViewHolder(view);
         //点击事件
-        holder.tv_otherName.setOnClickListener(new View.OnClickListener() {
+        holder.tv_otherTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int position = holder.getAdapterPosition();
-                MyMsgOther other = mOtherList.get(position);
-                Log.e(TAG, "onClick: "+"You have clicked view"+other.getName());
-                Toast.makeText(v.getContext(),"You have clicked view"+other.getName(),Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "onClick: "+"You have clicked view"+mOtherList[position]);
+                Toast.makeText(v.getContext(),"You have clicked view"+mOtherList[position],Toast.LENGTH_SHORT).show();
             }
         });
 
-        holder.iv_otherImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int position = holder.getAdapterPosition();
-                MyMsgOther other = mOtherList.get(position);
-                Log.e(TAG, "onClick: "+"You have clicked view"+other.getImageId() );
-                Toast.makeText(v.getContext(),"You have clicked view"+other.getImageId(),Toast.LENGTH_SHORT).show();
-            }
-        });
+//        holder.iv_otherImg.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int position = holder.getAdapterPosition();
+//                MyMsgOther other = mOtherList.get(position);
+//                Log.e(TAG, "onClick: "+"You have clicked view"+other.getImageId() );
+//                Toast.makeText(v.getContext(),"You have clicked view"+other.getImageId(),Toast.LENGTH_SHORT).show();
+//            }
+//        });
         return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        MyMsgOther other = mOtherList.get(i);
-        viewHolder.tv_otherName.setText(other.getName());
-        viewHolder.iv_otherImg.setImageResource(other.getImageId());
+        viewHolder.tv_otherTitle.setText(mOtherList[i]);
     }
 
     @Override
     public int getItemCount() {
-        return mOtherList.size();
+        return mOtherList.length;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         View otherView;
         View vLine;
-        TextView tv_otherName;
+        TextView tv_otherTitle;
         ImageView iv_otherImg;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             otherView = itemView;
-            tv_otherName = itemView.findViewById(R.id.my_msg_other_tv_name);
-            iv_otherImg = itemView.findViewById(R.id.my_msg_other_iv_image);
+            tv_otherTitle = itemView.findViewById(R.id.my_msg_other_tv_title);
+            iv_otherImg = itemView.findViewById(R.id.my_msg_other_iv_icon);
             vLine = itemView.findViewById(R.id.my_msg_other_view_line);
         }
     }
 
-    public MyMsgOtherAdapter(List<MyMsgOther> mOtherList) {
+    public MyMsgOtherAdapter(String[] mOtherList) {
         this.mOtherList = mOtherList;
     }
 }
