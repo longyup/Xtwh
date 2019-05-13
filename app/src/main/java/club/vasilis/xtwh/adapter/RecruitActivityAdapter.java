c0b4c2b6ea7d80b355b3317a5c6d2c2abe9e7043
zhilin <zhilin_98@163.com>
2019-05-13 21:08:21 +0800

    活动详情(修改)

--- app/src/main/java/club/vasilis/xtwh/adapter/RecruitActivityAdapter.java
+++ app/src/main/java/club/vasilis/xtwh/adapter/RecruitActivityAdapter.java
@@ -6,23 +6,26 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 import club.vasilis.xtwh.R;
 import club.vasilis.xtwh.activity.RecruitContentActivity;
 import club.vasilis.xtwh.domain.Recruit;
+import club.vasilis.xtwh.fragment.RecruitDailsFragment;
 
 public class RecruitActivityAdapter extends RecyclerView.Adapter<RecruitActivityAdapter.ViewHolder> {
-
     private List<Recruit> Datas;
 //    private List<String> distanceDatas;
 //    private List<String> regionActivityDatas;
 //    private List<String> numberDatas;
     private Context mContext;
     private LayoutInflater inflater;
+
 
     public RecruitActivityAdapter(List<Recruit> Datas, Context mContext) {
         this.Datas = Datas;
@@ -35,7 +38,7 @@ public class RecruitActivityAdapter extends RecyclerView.Adapter<RecruitActivity
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, final int i) {
         View view = LayoutInflater.from(mContext).inflate(R.layout.recruit_activity_item,viewGroup,false);
         final ViewHolder holder = new ViewHolder(view);
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -43,29 +46,31 @@ public class RecruitActivityAdapter extends RecyclerView.Adapter<RecruitActivity
             public void onClick(View v) {
                 Recruit recruit = Datas.get(holder.getAdapterPosition());
                 RecruitContentActivity.actionStart(mContext,recruit.getImgId(),recruit.getTitle(),recruit.getDistance(),recruit.getRegion(),recruit.getNumber());
-
-//                showNormalDialog();
             }
         });
+
+
         return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
     Recruit recruit = Datas.get(i);
     viewHolder.titletv.setText(recruit.getTitle());
     viewHolder.regiontv.setText(recruit.getRegion());
     viewHolder.distancetv.setText(recruit.getDistance());
     viewHolder.numbertv.setText(recruit.getNumber());
     viewHolder.resuritIv.setImageResource(recruit.getImgId());
-
-
     }
+
+
 
     @Override
     public int getItemCount() {
         return Datas.size();
     }
+
+
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView resuritIv;
@@ -74,7 +79,6 @@ public class RecruitActivityAdapter extends RecyclerView.Adapter<RecruitActivity
         TextView regiontv;
         TextView numbertv;
 
-
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             titletv = itemView.findViewById(R.id.recruit_title);
@@ -82,6 +86,9 @@ public class RecruitActivityAdapter extends RecyclerView.Adapter<RecruitActivity
             regiontv = itemView.findViewById(R.id.recurit_region_tv);
             numbertv = itemView.findViewById(R.id.recurit_peoplenumber_tv);
             resuritIv = itemView.findViewById(R.id.recurit_iv);
+
         }
     }
+
+
 }
