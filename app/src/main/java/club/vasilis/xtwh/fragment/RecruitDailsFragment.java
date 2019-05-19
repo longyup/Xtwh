0a80693f1d92611c8f51cecd1e075d6933d44d4a
Jim <1225469544@qq.com>
2019-05-19 20:22:38 +0800

    全部修改为ButterKnife

--- app/src/main/java/club/vasilis/xtwh/fragment/RecruitDailsFragment.java
+++ app/src/main/java/club/vasilis/xtwh/fragment/RecruitDailsFragment.java
@@ -2,11 +2,10 @@ package club.vasilis.xtwh.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
