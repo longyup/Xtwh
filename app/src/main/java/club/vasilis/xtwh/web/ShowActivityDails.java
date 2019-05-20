a14c513d23879e77b052131ee7afde3e4ba5a22a
ZhuZhiXin_glb <452299060@QQ.com>
2019-05-20 12:11:01 +0800

    合并

--- app/src/main/java/club/vasilis/xtwh/web/ShowActivityDails.java
+++ app/src/main/java/club/vasilis/xtwh/web/ShowActivityDails.java
@@ -7,11 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -20,6 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
