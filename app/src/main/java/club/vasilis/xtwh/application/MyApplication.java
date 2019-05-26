4d3ddc5bc24c0c42b0ab95f6b5b6775bb6253625
Vasilis <799453724@qq.com>
2019-05-26 19:21:19 +0800

    add client

---
+++ app/src/main/java/club/vasilis/xtwh/application/MyApplication.java
@@ -0,0 +1,25 @@
+package club.vasilis.xtwh.application;
+
+import android.app.Application;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * @author Vasilis
+ * @date 2019/5/26 * 19:16
+ */
+public class MyApplication extends Application {
+    private static final String HOST = "http://120.76.62.29:8080/10_NULL/";
+    private static OkHttpClient client = new OkHttpClient.Builder()
+            .readTimeout(2000L, TimeUnit.MILLISECONDS)
+            .callTimeout(2000L, TimeUnit.MILLISECONDS)
+            .build();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
