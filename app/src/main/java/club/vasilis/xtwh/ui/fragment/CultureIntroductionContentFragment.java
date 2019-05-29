285685eeb544a37036fa6c69ed5ebf6873f7329d
ZhuZhiXin_glb <452299060@QQ.com>
2019-05-29 13:45:11 +0800

    文化遗址详情页显title修改

--- app/src/main/java/club/vasilis/xtwh/ui/fragment/CultureIntroductionContentFragment.java
+++ app/src/main/java/club/vasilis/xtwh/ui/fragment/CultureIntroductionContentFragment.java
@@ -3,17 +3,21 @@ package club.vasilis.xtwh.ui.fragment;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import butterknife.Unbinder;
 import club.vasilis.xtwh.R;
 
@@ -26,11 +30,14 @@ public class CultureIntroductionContentFragment extends Fragment {
     @BindView(R.id.tv_cultureIntroduction_content)
     TextView tvCultureIntroductionContent;
 
-    @BindView(R.id.tv_cultureIntroduction_title)
-    TextView tvCultureIntroductionTitle;
+    @BindView(R.id.tb_cultureSites_infor)
+    Toolbar tbCultureSitesInfor;
+
+
+    /*@BindView(R.id.tv_cultureIntroduction_title)
+    TextView tvCultureIntroductionTitle;*/
 
     private Unbinder unbinder;
-
 
 
     @Nullable
@@ -38,27 +45,45 @@ public class CultureIntroductionContentFragment extends Fragment {
     public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
-
-
         view = inflater.inflate(R.layout.fragment_product_infor, container, false);
         unbinder = ButterKnife.bind(this, view);
         tvCultureIntroductionContent.setMovementMethod(ScrollingMovementMethod.getInstance());
-
-
+        ((AppCompatActivity) getActivity()).setSupportActionBar(tbCultureSitesInfor);
+        setHasOptionsMenu(true);
         return view;
+
+
     }
 
-    @OnClick(R.id.btn_content_back)
+   /* @OnClick(R.id.btn_content_back)
     void show(){
         getActivity().finish();
-    }
+    }*/
 
     public void refresh(String Title, String Content) {
         View ll_cultureIntroduction_visibility_layout = view.findViewById(R.id.ll_cultureIntroduction_visibility_layout);
         ll_cultureIntroduction_visibility_layout.setVisibility(View.VISIBLE);
         //刷新标题
-        tvCultureIntroductionTitle.setText(Title);
+        //tvCultureIntroductionTitle.setText(Title);
+        tbCultureSitesInfor.setTitle(Title);
         //刷新内容
         tvCultureIntroductionContent.setText(Content);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.tb_culture_sites_infor_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.back:
+                getActivity().finish();
+                break;
+                default:
+        }
+        return true;
     }
 }
